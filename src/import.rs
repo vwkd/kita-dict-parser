b4e26262a6efcd6b037ef9577be29109cad4b03b
vwkd <33468089+vwkd@users.noreply.github.com>
2023-02-04 18:09:51 +0200

    fix: filter verb lines, print error for each line instead of panic

--- src/import.rs
+++ src/import.rs
@@ -22,14 +22,20 @@ fn load_file(path: &str) -> io::Result<String> {
 
 fn preprocess(text: &str) -> Result<String, regex::Error> {
     let re_next_page = Regex::new(&format!(r"\n\n## {}", NEXT_PAGE))?;
-    let re_header_lines = Regex::new(r"(?m)^##.*\n")?;
-    let re_empty_lines = Regex::new(r"(?m)^\n")?;
-    let re_continued_lines = Regex::new(r"\n♦︎")?;
+    let s0 = re_next_page.split(text).next().expect(&format!("Page '{}' not found", NEXT_PAGE));
 
-    let s0 = re_next_page.split(text).next().unwrap();
+    let re_header_lines = Regex::new(r"(?m)^##.*\n")?;
     let s1 = re_header_lines.replace_all(s0, "");
+
+    let re_empty_lines = Regex::new(r"(?m)^\n")?;
     let s2 = re_empty_lines.replace_all(&s1, "");
+
+    let re_continued_lines = Regex::new(r"\n♦︎")?;
     let s3 = re_continued_lines.replace_all(&s2, "");
+
+    // todo: keep verb lines and parse
+    let re_verb_lines = Regex::new(r"(?m)^.*\n(^  .*\n)+")?;
+    let s4 = re_verb_lines.replace_all(&s3, "");
     
-    Ok(s3.into_owned())
+    Ok(s4.into_owned())
 }
--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,15 @@ use import::load_data;
 
 fn main() {
     let s = load_data().unwrap();
-
     
     for line in s.lines() {
-        let (_, entry) = parser::<()>(line).unwrap();
+        println!("{}", line);
 
-        println!("{:?}", entry);
+        match parser::<nom::error::Error<&str>>(line) {
+            Ok((_, entry)) => println!("{:?}", entry),
+            Err(e) => eprintln!("{}", e),
+        }
+
+        println!("");
     }
 }
