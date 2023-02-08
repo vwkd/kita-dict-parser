39a12e671607778efd9b9b8497e7c63c6ddd20e1
vwkd <33468089+vwkd@users.noreply.github.com>
2023-02-08 18:05:26 +0200

    feat: parse part of speech, categories and temporality tags

--- src/parser/general/expression.rs
+++ src/parser/general/expression.rs
@@ -1,26 +1,31 @@
 use nom::branch::alt;
 use nom::character::complete::char;
-use nom::combinator::{map, map_res};
+use nom::combinator::{map, map_res, opt};
 use nom::error::{context, VerboseError};
 use nom::multi::separated_list1;
-use nom::sequence::{separated_pair, terminated};
+use nom::sequence::{pair, separated_pair, terminated, tuple};
 use nom::IResult;
 
+use crate::parser::utils::separated_list2;
+
 use super::super::utils::KitaError;
+use super::category::{categories_parser, Categories};
 use super::character::{integer_parser, ws_parser};
+use super::part_of_speech::{part_of_speech_parser, PartOfSpeech};
 use super::reference::{reference_parser, Reference};
 use super::sentence_de::sentence_de_parser;
+use super::symbols::{temporality_parser, Temporality};
 use super::Index;
 
 /*
 Expression
     Usages
-    Usage
+    UsageTagged
 */
 #[derive(Debug)]
 pub enum Expression<'a> {
-    Usage(Usage<'a>),
     Usages(Usages<'a>),
+    UsageTagged(UsageTagged<'a>),
 }
 
 pub fn expression_parser(input: &str) -> IResult<&str, Expression, VerboseError<&str>> {
@@ -28,36 +33,117 @@ pub fn expression_parser(input: &str) -> IResult<&str, Expression, VerboseError<
         "expression",
         alt((
             map(usages_parser, Expression::Usages),
-            map(usage_parser, Expression::Usage),
+            map(usage_tagged_parser, Expression::UsageTagged),
         )),
     )(input)
 }
 
 /*
 Usages
-    UsageItem(1) ws UsageItem(2) (ws UsageItem(i))_i=3*
+    Tag UsageItem(1) ws UsageItem(2) (ws UsageItem(i))_i=3*
+    UsageItemTagged(1) ws UsageItemTagged(2) (ws UsageItemTagged(i))_i=3*
 */
 #[derive(Debug)]
-pub struct Usages<'a>(Vec<UsageItem<'a>>);
+pub enum Usages<'a> {
+    Common(Tag, Vec<UsageItem<'a>>),
+    Individual(Vec<UsageItemTagged<'a>>),
+}
 
 pub fn usages_parser(input: &str) -> IResult<&str, Usages, VerboseError<&str>> {
     context(
         "usages",
-        map_res(
-            separated_pair(
-                usage_item_parser,
-                ws_parser,
-                separated_list1(ws_parser, usage_item_parser),
+        alt((
+            map_res(
+                pair(tag_parser, separated_list2(ws_parser, usage_item_parser)),
+                |(tag, usage_items)| {
+                    // validate that integers are increasing with step 1
+                    let is_increasing = usage_items
+                        .iter()
+                        .enumerate()
+                        .all(|(i, val)| val.1 == i as u8 + 1);
+                    if !is_increasing {
+                        return Err(nom::Err::Error(KitaError::IncreasingUsagesList));
+                    }
+                    Ok(Usages::Common(tag, usage_items))
+                },
             ),
-            |(first, mut rest)| {
-                // hack for missing `separated_list2`
-                rest.insert(0, first);
-                // validate that integers are increasing with step 1
-                let is_increasing = rest.iter().enumerate().all(|(i, val)| val.1 == i as u8 + 1);
-                if !is_increasing {
-                    return Err(nom::Err::Error(KitaError::IncreasingUsagesList));
-                }
-                Ok(Usages(rest))
+            map_res(
+                separated_list2(ws_parser, usage_item_tagged_parser),
+                |usage_items_tagged| {
+                    // validate that integers are increasing with step 1
+                    let is_increasing = usage_items_tagged
+                        .iter()
+                        .enumerate()
+                        .all(|(i, val)| val.1 == i as u8 + 1);
+                    if !is_increasing {
+                        return Err(nom::Err::Error(KitaError::IncreasingUsagesList));
+                    }
+                    Ok(Usages::Individual(usage_items_tagged))
+                },
+            ),
+        )),
+    )(input)
+}
+
+/*
+UsageItemTagged(i)
+    i "." ws UsageTagged
+*/
+#[derive(Debug)]
+pub struct UsageItemTagged<'a>(UsageTagged<'a>, Index);
+
+pub fn usage_item_tagged_parser(input: &str) -> IResult<&str, UsageItemTagged, VerboseError<&str>> {
+    context(
+        "usage_item_tagged",
+        map(
+            separated_pair(
+                integer_parser,
+                terminated(char('.'), ws_parser),
+                usage_tagged_parser,
+            ),
+            |(index, usage_tagged)| UsageItemTagged(usage_tagged, index),
+        ),
+    )(input)
+}
+
+/*
+UsageTagged
+    Tag Usage
+*/
+#[derive(Debug)]
+pub struct UsageTagged<'a>(Tag, Usage<'a>);
+
+pub fn usage_tagged_parser(input: &str) -> IResult<&str, UsageTagged, VerboseError<&str>> {
+    context(
+        "usage_tagged",
+        map(tuple((tag_parser, usage_parser)), |(tag, usage)| {
+            UsageTagged(tag, usage)
+        }),
+    )(input)
+}
+
+/*
+Tag
+    (PartOfSpeech ws)? (Categories ws)? (Temporality ws)?
+*/
+#[derive(Debug)]
+pub struct Tag(
+    Option<PartOfSpeech>,
+    Option<Categories>,
+    Option<Temporality>,
+);
+
+pub fn tag_parser(input: &str) -> IResult<&str, Tag, VerboseError<&str>> {
+    context(
+        "tag",
+        map(
+            tuple((
+                opt(terminated(part_of_speech_parser, ws_parser)),
+                opt(terminated(categories_parser, ws_parser)),
+                opt(terminated(temporality_parser, ws_parser)),
+            )),
+            |(part_of_speech, categories, temporality)| {
+                Tag(part_of_speech, categories, temporality)
             },
         ),
     )(input)
@@ -84,7 +170,6 @@ pub fn usage_item_parser(input: &str) -> IResult<&str, UsageItem, VerboseError<&
     )(input)
 }
 
-// todo: part of speech (pos)
 /*
 Usage
     Definition (";" ws Definition)*
--- src/parser/utils.rs
+++ src/parser/utils.rs
@@ -1,4 +1,70 @@
+use nom::{
+    error::{ErrorKind, ParseError},
+    Err, IResult, InputLength, Parser,
+};
+
 #[derive(Debug)]
 pub enum KitaError {
     IncreasingUsagesList,
+}
+
+/// like [`separated_list1`]
+/// todo: replace with multi-range `separated_list` once implemented [nom#1613](https://github.com/rust-bakery/nom/issues/1613#issuecomment-1386162101)
+pub fn separated_list2<I, O, O2, E, F, G>(
+    mut sep: G,
+    mut f: F,
+) -> impl FnMut(I) -> IResult<I, Vec<O>, E>
+where
+    I: Clone + InputLength,
+    F: Parser<I, O, E>,
+    G: Parser<I, O2, E>,
+    E: ParseError<I>,
+{
+    move |mut i: I| {
+        let mut res = Vec::new();
+
+        // Parse the first element
+        match f.parse(i.clone()) {
+            Err(e) => return Err(e),
+            Ok((i1, o)) => {
+                res.push(o);
+                i = i1;
+            }
+        }
+
+        // Parse the second element
+        match sep.parse(i.clone()) {
+            Err(e) => return Err(e),
+            Ok((i2, _)) => match f.parse(i2.clone()) {
+                Err(e) => return Err(e),
+                Ok((i2, o)) => {
+                    res.push(o);
+                    i = i2;
+                }
+            },
+        }
+
+        loop {
+            let len = i.input_len();
+            match sep.parse(i.clone()) {
+                Err(Err::Error(_)) => return Ok((i, res)),
+                Err(e) => return Err(e),
+                Ok((i1, _)) => {
+                    // infinite loop check: the parser must always consume
+                    if i1.input_len() == len {
+                        return Err(Err::Error(E::from_error_kind(i1, ErrorKind::SeparatedList)));
+                    }
+
+                    match f.parse(i1.clone()) {
+                        Err(Err::Error(_)) => return Ok((i, res)),
+                        Err(e) => return Err(e),
+                        Ok((i2, o)) => {
+                            res.push(o);
+                            i = i2;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
