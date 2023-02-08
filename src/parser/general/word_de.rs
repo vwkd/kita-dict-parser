98868ac5460aa1a2f900826b82beca52fc3b03a7
vwkd <33468089+vwkd@users.noreply.github.com>
2023-02-08 16:12:02 +0200

    refactor: move tests into tests module

--- src/parser/general/word_de.rs
+++ src/parser/general/word_de.rs
@@ -15,18 +15,6 @@ pub fn word_de_small_parser(input: &str) -> IResult<&str, &str, VerboseError<&st
     context("word_de_small", take_while1(is_char_de_small))(input)
 }
 
-#[test]
-fn test_word_de_small_parser() {
-    let a = word_de_small_parser("bär");
-    assert!(a.is_ok());
-
-    let b = word_de_small_parser(" bär");
-    assert!(b.is_err());
-
-    let c = word_de_small_parser("Bär");
-    assert!(c.is_err());
-}
-
 // note: require at least two letters
 /*
 WordDeBig
@@ -40,18 +28,6 @@ pub fn word_de_big_parser(input: &str) -> IResult<&str, &str, VerboseError<&str>
             take_while1(is_char_de_small),
         ))),
     )(input)
-}
-
-#[test]
-fn test_word_de_big_parser() {
-    let a = word_de_big_parser("Bär");
-    assert!(a.is_ok());
-
-    let b = word_de_big_parser(" Bär");
-    assert!(b.is_err());
-
-    let c = word_de_big_parser("bär");
-    assert!(c.is_err());
 }
 
 /*
@@ -69,25 +45,6 @@ fn is_char_de_small(c: char) -> bool {
     }
 }
 
-#[test]
-fn test_is_char_de_small() {
-    assert!(is_char_de_small('a'));
-    assert!(is_char_de_small('j'));
-    assert!(is_char_de_small('z'));
-    assert!(is_char_de_small('ä'));
-    assert!(is_char_de_small('ö'));
-    assert!(is_char_de_small('ü'));
-    assert!(is_char_de_small('ß'));
-
-    assert!(!is_char_de_small('A'));
-    assert!(!is_char_de_small('J'));
-    assert!(!is_char_de_small('Z'));
-    assert!(!is_char_de_small('Ä'));
-    assert!(!is_char_de_small('Ö'));
-    assert!(!is_char_de_small('Ü'));
-    assert!(!is_char_de_small('ẞ'));
-}
-
 /*
 CharDeBig
     UNICODE_GERMAN_BIG_CHARACTER
@@ -103,21 +60,69 @@ fn is_char_de_big(c: char) -> bool {
     }
 }
 
-#[test]
-fn test_is_char_de_big() {
-    assert!(is_char_de_big('A'));
-    assert!(is_char_de_big('J'));
-    assert!(is_char_de_big('Z'));
-    assert!(is_char_de_big('Ä'));
-    assert!(is_char_de_big('Ö'));
-    assert!(is_char_de_big('Ü'));
-    assert!(is_char_de_big('ẞ'));
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    assert!(!is_char_de_big('a'));
-    assert!(!is_char_de_big('j'));
-    assert!(!is_char_de_big('z'));
-    assert!(!is_char_de_big('ä'));
-    assert!(!is_char_de_big('ö'));
-    assert!(!is_char_de_big('ü'));
-    assert!(!is_char_de_big('ß'));
+    #[test]
+    fn test_word_de_small_parser() {
+        let a = word_de_small_parser("bär");
+        assert!(a.is_ok());
+
+        let b = word_de_small_parser(" bär");
+        assert!(b.is_err());
+
+        let c = word_de_small_parser("Bär");
+        assert!(c.is_err());
+    }
+
+    #[test]
+    fn test_word_de_big_parser() {
+        let a = word_de_big_parser("Bär");
+        assert!(a.is_ok());
+
+        let b = word_de_big_parser(" Bär");
+        assert!(b.is_err());
+
+        let c = word_de_big_parser("bär");
+        assert!(c.is_err());
+    }
+
+    #[test]
+    fn test_is_char_de_small() {
+        assert!(is_char_de_small('a'));
+        assert!(is_char_de_small('j'));
+        assert!(is_char_de_small('z'));
+        assert!(is_char_de_small('ä'));
+        assert!(is_char_de_small('ö'));
+        assert!(is_char_de_small('ü'));
+        assert!(is_char_de_small('ß'));
+
+        assert!(!is_char_de_small('A'));
+        assert!(!is_char_de_small('J'));
+        assert!(!is_char_de_small('Z'));
+        assert!(!is_char_de_small('Ä'));
+        assert!(!is_char_de_small('Ö'));
+        assert!(!is_char_de_small('Ü'));
+        assert!(!is_char_de_small('ẞ'));
+    }
+
+    #[test]
+    fn test_is_char_de_big() {
+        assert!(is_char_de_big('A'));
+        assert!(is_char_de_big('J'));
+        assert!(is_char_de_big('Z'));
+        assert!(is_char_de_big('Ä'));
+        assert!(is_char_de_big('Ö'));
+        assert!(is_char_de_big('Ü'));
+        assert!(is_char_de_big('ẞ'));
+
+        assert!(!is_char_de_big('a'));
+        assert!(!is_char_de_big('j'));
+        assert!(!is_char_de_big('z'));
+        assert!(!is_char_de_big('ä'));
+        assert!(!is_char_de_big('ö'));
+        assert!(!is_char_de_big('ü'));
+        assert!(!is_char_de_big('ß'));
+    }
 }
--- src/parser/general/word_ka.rs
+++ src/parser/general/word_ka.rs
@@ -13,19 +13,6 @@ pub fn word_ka_small_parser(input: &str) -> IResult<&str, &str, VerboseError<&st
     context("word_ka_small", take_while1(is_char_ka))(input)
 }
 
-#[test]
-fn test_word_ka_small_parser() {
-    let a = word_ka_small_parser("კატა");
-    assert!(a.is_ok());
-
-    let b = word_ka_small_parser(" კატა");
-    assert!(b.is_err());
-
-    // beware: first is capital letter, different from small letters!
-    let c = word_ka_small_parser("Კატა");
-    assert!(c.is_err());
-}
-
 /*
 CharKaSmall
     UNICODE_GEORGIAN_CHARACTER
@@ -37,22 +24,40 @@ fn is_char_ka(c: char) -> bool {
     }
 }
 
-#[test]
-fn test_is_char_ka() {
-    assert!(is_char_ka('ა'));
-    assert!(is_char_ka('ჯ'));
-    assert!(is_char_ka('ჰ'));
-    assert!(is_char_ka('პ'));
-    assert!(is_char_ka('ყ'));
-    assert!(is_char_ka('ტ'));
-    assert!(is_char_ka('წ'));
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    // beware: capital letters, different from small letters!
-    assert!(!is_char_ka('Ა'));
-    assert!(!is_char_ka('Ჯ'));
-    assert!(!is_char_ka('Ჰ'));
-    assert!(!is_char_ka('Პ'));
-    assert!(!is_char_ka('Ყ'));
-    assert!(!is_char_ka('Ტ'));
-    assert!(!is_char_ka('Წ'));
+    #[test]
+    fn test_word_ka_small_parser() {
+        let a = word_ka_small_parser("კატა");
+        assert!(a.is_ok());
+
+        let b = word_ka_small_parser(" კატა");
+        assert!(b.is_err());
+
+        // beware: first is capital letter, different from small letters!
+        let c = word_ka_small_parser("Კატა");
+        assert!(c.is_err());
+    }
+
+    #[test]
+    fn test_is_char_ka() {
+        assert!(is_char_ka('ა'));
+        assert!(is_char_ka('ჯ'));
+        assert!(is_char_ka('ჰ'));
+        assert!(is_char_ka('პ'));
+        assert!(is_char_ka('ყ'));
+        assert!(is_char_ka('ტ'));
+        assert!(is_char_ka('წ'));
+
+        // beware: capital letters, different from small letters!
+        assert!(!is_char_ka('Ა'));
+        assert!(!is_char_ka('Ჯ'));
+        assert!(!is_char_ka('Ჰ'));
+        assert!(!is_char_ka('Პ'));
+        assert!(!is_char_ka('Ყ'));
+        assert!(!is_char_ka('Ტ'));
+        assert!(!is_char_ka('Წ'));
+    }
 }
