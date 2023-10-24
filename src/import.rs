83d0fa085b04c1a95ece87f0894f21f1235909b0
vwkd <33468089+vwkd@users.noreply.github.com>
2023-10-25 02:17:45 +0400

    import: simplify
    - single Vec, can get entry number from index
    - proper errors
    - single regex, match entries instead of delimiters
    - polish, use variable shadowing instead of multiple variables, better variable names

--- src/import.rs
+++ src/import.rs
@@ -1,55 +1,89 @@
-use itertools::Itertools;
 use regex::Regex;
-use std::fs;
-use std::io;
+use std::{error, fmt, fs, io};
 
-const PATH: &str = "../kita-dict-data/src/dict.txt";
+const DICT_FILEPATH: &str = "../kita-dict-data/src/dict.txt";
 
-/// first field is collection of all entries except verbs,
-/// second is collection of verb entries
-pub struct DictData(pub Vec<String>, pub Vec<String>);
-
-pub fn load_data(next_page: &str) -> io::Result<DictData> {
-    let text = load_file(PATH)?;
-    let data = preprocess(&text, next_page);
-    Ok(data)
+#[derive(Debug)]
+pub enum Entry {
+    Verb(String),
+    Other(String),
 }
 
-fn load_file(path: &str) -> io::Result<String> {
-    fs::read_to_string(path)
+pub type Dict = Vec<Entry>;
+
+#[derive(Debug)]
+pub enum ImportError {
+    LoadingFile(io::Error),
+    PageNotFound(String),
+    // GettingEntries,
 }
 
-/// Returns a tuple with the dict text without verb lines and the dict text with verb lines
-fn preprocess(text: &str, next_page: &str) -> DictData {
+impl fmt::Display for ImportError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            ImportError::LoadingFile(..) => write!(f, "can't load file"),
+            ImportError::PageNotFound(ref page) => write!(f, "can't find page '{}'", page),
+            // ImportError::GettingEntries =>
+            //     write!(f, "can't get entries"),
+        }
+    }
+}
+
+impl error::Error for ImportError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        match *self {
+            ImportError::LoadingFile(ref e) => Some(e),
+            ImportError::PageNotFound(..) => None,
+            // ImportError::GettingEntries => None,
+        }
+    }
+}
+
+impl From<io::Error> for ImportError {
+    fn from(err: io::Error) -> ImportError {
+        ImportError::LoadingFile(err)
+    }
+}
+
+pub fn load_dict(next_page: &str) -> Result<Dict, ImportError> {
+    let text = fs::read_to_string(DICT_FILEPATH)?;
+    get_entries(&text, next_page)
+}
+
+/// Get entries from dict files
+/// - split page
+/// - filter out header lines
+/// - filter out empty lines
+/// - merge page breaks
+/// - partition into entries
+pub fn get_entries(text: &str, next_page: &str) -> Result<Dict, ImportError> {
     let re_next_page = Regex::new(&format!(r"\n\n## {}", next_page)).expect("Invalid Regex");
-    let text0 = re_next_page
+    let text = re_next_page
         .split(text)
         .next()
-        .expect(&format!("Page '{}' not found", next_page));
+        .ok_or_else(|| ImportError::PageNotFound(next_page.to_owned()))?;
 
+    // todo: find way to use slices to avoid allocations
     let re_header_lines = Regex::new(r"(?m)^##.*\n").expect("Invalid Regex");
-    let text1 = re_header_lines.replace_all(text0, "");
+    let text = re_header_lines.replace_all(text, "");
 
     let re_empty_lines = Regex::new(r"(?m)^\n").expect("Invalid Regex");
-    let text2 = re_empty_lines.replace_all(&text1, "");
+    let text = re_empty_lines.replace_all(&text, "");
 
     let re_continued_lines = Regex::new(r"\n♦︎").expect("Invalid Regex");
-    let text = re_continued_lines.replace_all(&text2, "");
+    let text = re_continued_lines.replace_all(&text, "");
 
-    let re_verb_lines = Regex::new(r"(?m)^.*\n(^  .*\n)*(^  .*)").expect("Invalid Regex");
+    let re_entry = Regex::new(r"(?m)^\S.+(?:\n  .+)*$").expect("Invalid Regex");
 
-    // let text_noverbs = re_verb_lines.replace_all(&text, "").to_string();
-    let text_noverbs = re_verb_lines.split(&text).join("");
-    let lines_noverbs: Vec<String> = text_noverbs
-        .lines()
-        .filter(|l| !l.is_empty())
-        .map(|l| l.to_owned())
-        .collect();
-
-    let lines_verbs: Vec<String> = re_verb_lines
+    // todo: make sure matching instead of splitting actually covers the whole string, i.e. no unmatched parts due to errors in the source text
+    Ok(re_entry
         .find_iter(&text)
-        .map(|m| m.as_str().to_owned())
-        .collect();
-
-    DictData(lines_noverbs, lines_verbs)
+        .map(|entry| {
+            if entry.as_str().contains('\n') {
+                Entry::Verb(entry.as_str().to_owned())
+            } else {
+                Entry::Other(entry.as_str().to_owned())
+            }
+        })
+        .collect())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,28 @@
 mod import;
 mod parser;
 
-use import::{load_data, DictData};
-use parser::general;
-use parser::verb;
+use import::{load_dict, Entry};
+use parser::{general, verb};
 
 fn main() {
     let next_page = std::env::var("NEXT_PAGE").unwrap_or("1/39".to_owned());
 
     println!("Parsing dict until page {}...", next_page);
 
-    let DictData(lines_noverbs, lines_verbs) = load_data(&next_page).expect("Error loading data");
+    let dict = load_dict(&next_page).expect("Error getting entries");
 
-    let noverbs_total = lines_noverbs.len() as f32;
-    let mut noverbs_success = 0.;
-
-    println!("--------- NOVERBS ---------");
-
-    for (index, line) in lines_noverbs.into_iter().enumerate() {
-        // todo: parse skipped lines
-        if line.contains(['|', '~', '=']) || line.contains("...") {
-            continue;
-        }
-
-        let entry = general::parse(&line);
-
+    for (index, entry) in dict.into_iter().enumerate() {
         match entry {
-            Err(e) => {
-                eprintln!("{index}: {line}");
-                eprintln!("{:#?}\n", e);
+            Entry::Verb(ref line) => {
+                continue;
+                // println!("{index}: {line}");
+                // let parsed_entry = verb::parse(line);
             }
-            Ok(entry) => {
-                noverbs_success += 1.;
+            Entry::Other(ref line) => {
                 println!("{index}: {line}");
-                println!("{:?}\n", entry);
+                let parsed_entry = general::parse(line);
+                println!("{:?}\n", parsed_entry);
             }
         }
     }
-
-    let noverbs_success_percent = noverbs_success / noverbs_total * 100.;
-    println!(
-        "Noverbs parsed successfully: {:.1}% ({}/{})",
-        noverbs_success_percent, noverbs_success, noverbs_total
-    );
-
-    let verbs_total = lines_verbs.len() as f32;
-    let mut verbs_success = 0.;
-
-    println!("--------- VERBS ---------");
-
-    for (index, line) in lines_verbs.into_iter().enumerate() {
-        let entry = verb::parse(&line);
-
-        match entry {
-            Err(e) => {
-                eprintln!("{index}: {line}");
-                eprintln!("{:#?}\n", e);
-            }
-            Ok(entry) => {
-                verbs_success += 1.;
-                println!("{index}: {line}");
-                println!("{:?}\n", entry);
-            }
-        }
-    }
-
-    let verbs_success_percent = verbs_success / verbs_total * 100.;
-    println!(
-        "Verbs parsed successfully: {:.1}% ({}/{})",
-        verbs_success_percent, verbs_success, verbs_total
-    );
 }
